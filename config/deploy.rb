b4cbc7ba514b385e1f237a3b6ba80abe8f37b845
ram <ramusus@gmail.com>
2012-10-26 13:09:00 +0400

    added unicorn config file

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,22 +3,23 @@ require 'rvm/capistrano'
 require 'bundler/capistrano'
 #require 'capistrano/deepmodules'
 
-set :application, "gaidarfund-awards"
-set :deploy_to, "/data/www/award.gaidarfund.ru"
+set :application, "gaidarfund"
+set :deploy_to, "/srv/www/apps/gaidarfund"
 set :deploy_via, :remote_cache
 set :use_sudo, true
 set :user, "www"
+set :unicorn_conf, '/srv/www/apps/gaidarfund/current/config/unicorn.rb'
 
 set :rvm_ruby_string, "1.9.2"
 set :rvm_type, :user
 
 set :scm, "git"
-set :repository,  "git@ram.unfuddle.com:ram/#{application}.git"
+set :repository,  "git@ram.unfuddle.com:ram/gaidarfundproject.git"
 set :branch, "master"
 
-role :web, "lectures.dev.infolio.ru"                          # Your HTTP server, Apache/etc
-role :app, "lectures.dev.infolio.ru"                          # This may be the same as your `Web` server
-role :db,  "lectures.dev.infolio.ru", :primary => true # This is where Rails migrations will run
+role :web, "beta.gaidarfund.ru"                          # Your HTTP server, Apache/etc
+role :app, "beta.gaidarfund.ru"                          # This may be the same as your `Web` server
+role :db,  "beta.gaidarfund.ru", :primary => true # This is where Rails migrations will run
 
 default_run_options[:pty] = true
 
@@ -26,13 +27,13 @@ after "bundle:install", "deploy:auto_migrate"
 
 namespace :deploy do
   task :start do
-    run "cd #{deploy_to}/current && ./server start"
+    run "bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
   end
   task :stop do
-    run "cd #{deploy_to}/current && ./server stop"
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
    end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "cd #{deploy_to}/current && ./server restart"
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
   end
   task :auto_migrate do
     rake = fetch(:rake, "rake")
