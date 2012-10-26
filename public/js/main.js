624f296052feaea2f56ae830f8f30a2ad015cd43
ram <ramusus@gmail.com>
2012-10-27 00:56:59 +0400

    added main.js

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.b-hint-label').hints();
+});
+
+String.prototype.supplant = function (o) {
+    return this.replace(/{([^{}]*)}/g,
+        function (a, b) {
+            var r = o[b];
+            return typeof r === 'string' || typeof r === 'number' ? r : a;
+        }
+    );
+};
