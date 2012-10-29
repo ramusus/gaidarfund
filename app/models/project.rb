2624e1519aea223d7b2466f5653694e5a5dc0838
ram <ramusus@gmail.com>
2012-10-29 15:11:24 +0400

    updated datas and project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,6 @@ class Project < ActiveRecord::Base
   attr_accessible :title, :title_short, :subdomain, :color, :sign, :about_title, :news_title, :status, :likes, :partners, :counters, :right_block, :projects, :background_image, :logo_image, :html_block
 
   has_many :articles
-  has_many :events
 
   has_attached_file :background_image
   has_attached_file :logo_image
