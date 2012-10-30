2c3be4acef02df0ec40b7239a0652d1ceb788e1c
ram <ramusus@gmail.com>
2012-10-30 16:16:51 +0400

    fixed project_header

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121030113643) do
+ActiveRecord::Schema.define(:version => 20121030121625) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
