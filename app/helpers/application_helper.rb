dea1424f6b195177b14a30b9a12a267a2a21ecde
ram <ramusus@gmail.com>
2012-10-30 16:33:58 +0400

    fixed urls of project articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     root_url(:subdomain => project.subdomain)
   end
 
+  def article_path(options)
+    if options.project
+      polymorphic_url(options, :subdomain => options.project.subdomain)
+    else
+      super
+    end
+  end
+
 end
