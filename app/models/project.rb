7da35e94a4288a4ab1b30f3f843e6a8729dc4257
ram <ramusus@gmail.com>
2012-11-01 17:33:40 +0400

    changed color of articlelinks

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,14 +9,13 @@ class Project < ActiveRecord::Base
   has_attached_file :background_image
   has_attached_file :logo_image
   has_attached_file :logo_small_image, :styles => {:thumb => "220x111"}
-
   attr_accessor :delete_css
-  before_validation { self.css = nil if self.delete_css == '1' }
   attr_accessor :delete_background_image
-  before_validation { self.background_image = nil if self.delete_background_image == '1' }
   attr_accessor :delete_logo_image
-  before_validation { self.logo_image = nil if self.delete_logo_image == '1' }
   attr_accessor :delete_logo_small_image
+  before_validation { self.css = nil if self.delete_css == '1' }
+  before_validation { self.background_image = nil if self.delete_background_image == '1' }
+  before_validation { self.logo_image = nil if self.delete_logo_image == '1' }
   before_validation { self.logo_small_image = nil if self.delete_logo_small_image == '1' }
 
   def to_s
