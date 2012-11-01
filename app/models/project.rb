17f4776be42c56fc794169438a23b1160553af21
ram <ramusus@gmail.com>
2012-11-01 04:13:09 +0400

    returned logo image

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :title, :title_short, :subdomain, :color, :core, :sign, :about_title, :news_title, :status, :likes, :partners, :counters, :right_block, :projects, :css, :background_image, :logo_image, :html_block, :logo_small_image, :title_seo
+  attr_accessible :title, :title_short, :subdomain, :color, :core, :sign, :about_title, :news_title, :status, :likes, :partners, :counters, :right_block, :projects, :css, :background_image, :logo_image, :html_block, :logo_small_image, :title_seo, :delete_css, :delete_background_image, :delete_logo_image, :delete_logo_small_image
 
   has_many :articles
   has_many :pages
@@ -8,6 +8,15 @@ class Project < ActiveRecord::Base
   has_attached_file :background_image
   has_attached_file :logo_image
   has_attached_file :logo_small_image, :styles => {:thumb => "220x111"}
+
+  attr_accessor :delete_css
+  before_validation { self.css = nil if self.delete_css == '1' }
+  attr_accessor :delete_background_image
+  before_validation { self.background_image = nil if self.delete_background_image == '1' }
+  attr_accessor :delete_logo_image
+  before_validation { self.logo_image = nil if self.delete_logo_image == '1' }
+  attr_accessor :delete_logo_small_image
+  before_validation { self.logo_small_image = nil if self.delete_logo_small_image == '1' }
 
   def to_s
     self.title_short or self.title
