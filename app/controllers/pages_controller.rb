a332b62b73428149b0d170022acc9a5f6115dcd2
ram <ramusus@gmail.com>
2012-11-04 15:39:25 +0400

    fixed old_id redirecting #2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,9 @@
 class PagesController < ApplicationController
   def show
-    pages = Page.all
     if not request.subdomain.empty? and project = Project.find_by_subdomain(request.subdomain)
-      pages = pages.where("project_id = ?", project.id)
+      pages = Page.where("project_id = ?", project.id)
     else
-      pages = pages.where("project_id is NULL")
+      pages = Page.where("project_id is NULL")
     end
 
     @page = pages.find_by_slug(params[:slug] || '') or not_found
