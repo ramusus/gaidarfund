66a3aa31a977eb0913f47121b4fcc34c709e2672
ram <ramusus@gmail.com>
2013-01-15 20:19:05 +0530

    small fixes

--- app/models/project_archive_period.rb
+++ app/models/project_archive_period.rb
@@ -1,5 +1,5 @@
 class ProjectArchivePeriod < ActiveRecord::Base
-  default_scope :order => 'date_start ASC'
+  default_scope :order => 'date_start DESC'
   attr_accessible :id, :name, :date_start, :date_end, :project_id
 
   belongs_to :project
